3c9019f83a56a9c6acd292e11796353191dc9902
Car_pe <Tu.shaWw1219@gmail.com>
2022-01-21 22:32:11 +0800

    CS229 Ex5 Bias/Variances

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -46,9 +46,6 @@ for i = 1:length(lambda_vec)
    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
-
-
-
 % =========================================================================
 
 end
