fa06ac713e0da903160bf59cdd443096759c8d67
Car_pe <Tu.shaWw1219@gmail.com>
2022-02-18 20:58:28 +0800

    C

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0.01;
+sigma = 0.01;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,12 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_test = [0.01;0.03;0.1;0.3;1;3;10;30];
+Sigma_test = [0.01;0.03;0.1;0.3;1;3;10;30];
+m = size(C_test, 1);
+Value = zeros(m^2,1);
 
+for i = 1:m
+  for j = 1:m
+    C = C_test(i);
+    sigma = Sigma_test(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+    predictions = svmPredict(model, Xval);
+    Value((i-1)*m+j) = mean(double(predictions ~= yval));
+  end
+end
+% 这里的整数判定很奇怪
 
-
-
-
-
+[minValue, minIndex] = min(Value);
+index1 = floor(minIndex / m);
+index2 = minIndex - (index1) * m;
+C = C_test(index1);
+sigma = Sigma_test(index2);
 
 % =========================================================================
 
